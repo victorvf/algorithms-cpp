876e88d3b762d465cb14e9314bc39b36c8fa16cf
˜victorvf˜ <victorvitoria11@hotmail.com>
2022-01-15 22:43:28 -0300

    feature: insertion sort

---
+++ insertion_sort.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void print_array (string title, int (&arr) [6]) {
+    cout << title << " array: [";
+    for (int y = 0; y < 6; y++) {
+        if (y != 5) {
+            cout << arr[y] << ", ";
+        } else {
+            cout << arr[y];
+        }
+    }
+    cout << "]" << endl;
+}
+
+// insertion sort
+int main () {
+    int i;
+    int key;
+    // int arr [6] = {5,2,4,6,1,3};
+    int arr [6] = {31,41,59,26,41,58};
+
+    print_array("Initial", arr);
+
+    int arr_length = sizeof(arr)/sizeof(arr[0]);
+    for (int j = 1; j < arr_length; j++) {
+        key = arr[j];
+        i = j - 1;
+
+        // loop invariant
+        while (i >= 0 && arr[i] > key) {
+            arr[i+1] = arr[i];
+            i--;
+        }
+
+        arr[i+1] = key;
+    }
+
+    print_array("Sorted", arr);
+
+    return 0;
+}
